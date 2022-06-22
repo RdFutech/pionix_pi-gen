a8cc0f6caac97da9553c12476ba8b2925dd2ed95
Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>
2022-06-22 17:44:08 +0200

    Enable cross compiling by preparing a toolchain configuration
    Uses the rootfs as basis for the sysroot, installing missing openssl and sqlite header in there
    Additionally cross-compiles boost and makes the BOOST_ROOT known to cmake
    
    Signed-off-by: Kai-Uwe Hermann <kai-uwe.hermann@pionix.de>

--- stage2/09-install-everest/01-run.sh
+++ stage2/09-install-everest/01-run.sh
@@ -4,12 +4,25 @@ install -m 755 files/stm32flash "${ROOTFS_DIR}/usr/bin"
 #install -m 644 files/boot-mark-good.service "${ROOTFS_DIR}/lib/systemd/system/"
 
 mkdir -p $WORK_DIR/everest
+cp files/prepare-sysroot.sh $WORK_DIR/everest
+mkdir -p $WORK_DIR/everest/sysroot/usr
+cp -R "${ROOTFS_DIR}/usr/include" "$WORK_DIR/everest/sysroot/usr"
+cp -R "${ROOTFS_DIR}/usr/lib" "$WORK_DIR/everest/sysroot/usr"
+ls -l "$WORK_DIR/everest/sysroot/usr"
+(
+    # prepare sysroot
+    cd $WORK_DIR/everest
+    ./prepare-sysroot.sh
+)
+
 (
 cd $WORK_DIR/everest
 git clone git@github.com:PionixInternal/everest-deploy-devkit.git || true
 cd everest-deploy-devkit/belayboxr1
+git checkout kh-cross-compiling
 git pull || true
 mkdir -p work
+cp $WORK_DIR/everest/toolchain.cmake work
 ./build_and_install.sh work ${ROOTFS_DIR}
 )
 on_chroot <<EOF
---
+++ stage2/09-install-everest/files/prepare-sysroot.sh
@@ -0,0 +1,104 @@
+#!/bin/bash
+
+
+# prerequisites:
+# sudo apt-get install g++-arm-linux-gnueabihf
+# sudo apt-get install gcc-arm-linux-gnueabihf
+# sudo apt-get install binutils-arm-linux-gnueabihf
+# TODO: FIXME: make sure to find the appropriate versions of openssl, sqlite and boost in the image sysroot and download them based on that
+
+MAIN_DIR=$(pwd)
+
+OUT_DIR="$MAIN_DIR/out"
+SQLITE_DIR="$OUT_DIR/sqlite"
+OPENSSL_INSTALL_DIR="$OUT_DIR/openssl"
+
+echo "$MAIN_DIR $OUT_DIR $SQLITE_DIR $OPENSSL_INSTALL_DIR"
+
+SYSROOT_DIR="$MAIN_DIR/sysroot"
+BOOST_DIR="$MAIN_DIR/boost_1_74_0"
+
+cat >toolchain.cmake <<EOL
+set(CMAKE_SYSTEM_NAME Linux)
+set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_C_COMPILER "/usr/bin/arm-linux-gnueabihf-gcc")
+set(CMAKE_CXX_COMPILER "/usr/bin/arm-linux-gnueabihf-g++")
+set(CMAKE_FIND_ROOT_PATH ${SYSROOT_DIR})
+set(CMAKE_FIND_ROOT_PATH_MODE_PROGRAM NEVER)
+set(BOOST_ROOT "${BOOST_DIR}")
+set(Boost_INCLUDE_DIR "${BOOST_DIR}")
+include_directories("${BOOST_DIR}" "${SYSROOT_DIR}/usr/include")
+link_directories("${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf")
+set(Boost_NO_SYSTEM_PATHS ON)
+set(BOOST_LIBRARYDIR "${SYSROOT_DIR}/usr/lib/arm-linux-gnueabihf")
+
+EOL
+
+if [[ -f sysroot_prepared ]]
+then
+    echo "sysroot already prepared"
+    exit 0
+fi
+
+ls -l "$SYSROOT_DIR"
+
+# TODO only build these things when the respective "out" folders are empty
+
+# cross compile sqlite3
+if [[ ! -f sqlite-autoconf-3340100.tar.gz ]]
+then
+    wget https://www.sqlite.org/2021/sqlite-autoconf-3340100.tar.gz
+    tar xvzf sqlite-autoconf-3340100.tar.gz
+fi
+
+cd sqlite-autoconf-3340100
+./configure --host=arm-linux --prefix="$SQLITE_DIR" CC=/usr/bin/arm-linux-gnueabihf-gcc
+make -j8
+make install
+
+cp $SQLITE_DIR/include/*.h "$SYSROOT_DIR/usr/include/"
+cp $SQLITE_DIR/lib/lib* "$SYSROOT_DIR/usr/lib/"
+
+cd $MAIN_DIR
+
+# cross compile openssl
+if [[ ! -d openssl ]]
+then
+    git clone https://github.com/openssl/openssl.git
+    cd openssl
+    git checkout OpenSSL_1_1_1-stable 
+    cd $MAIN_DIR
+fi
+
+cd openssl
+./Configure linux-generic32 shared --prefix=$OPENSSL_INSTALL_DIR --openssldir=$OPENSSL_INSTALL_DIR/openssl --cross-compile-prefix=/usr/bin/arm-linux-gnueabihf-
+make depend -j8
+make -j8
+make install
+
+cp -R $OPENSSL_INSTALL_DIR/include/openssl "$SYSROOT_DIR/usr/include/"
+cp $OPENSSL_INSTALL_DIR/lib/lib* "$SYSROOT_DIR/usr/lib/"
+
+cd $MAIN_DIR
+
+#cross compile boost
+if [[ ! -f boost_1_74_0.tar.bz2 ]]
+then
+    wget https://sourceforge.net/projects/boost/files/boost/1.74.0/boost_1_74_0.tar.bz2
+    tar xjvf boost_1_74_0.tar.bz2
+fi
+
+cd boost_1_74_0
+
+./bootstrap.sh
+
+CONFIG_JAM=$(mktemp)
+
+echo "using gcc : arm : /usr/lib/ccache/arm-linux-gnueabihf-g++ ;" > "$CONFIG_JAM"
+
+./b2 --user-config="$CONFIG_JAM" --with-system --with-filesystem --with-exception --with-log --with-program_options --with-date_time --no-samples --no-tests toolset=gcc-arm link=shared cxxflags=-fPIC
+
+cd $MAIN_DIR
+
+touch sysroot_prepared
+
