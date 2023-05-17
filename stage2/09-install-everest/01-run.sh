0ab3fee245a43ba0568d70b285ce1ebe1306d908
Cornelius Claussen <cc@pionix.de>
2023-05-17 08:26:48 +0200

    remove ocpp symlink, dir structure has changed
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- stage2/09-install-everest/01-run.sh
+++ stage2/09-install-everest/01-run.sh
@@ -19,7 +19,7 @@ systemctl enable everest.service
 systemctl enable everest-rpi.service
 systemctl enable everest-dev.service
 systemctl enable mosquitto-config-init.service
-ln -s /mnt/user_data/user-config/ocpp /opt/everest/share/everest/ocpp/
+#ln -s /mnt/user_data/user-config/ocpp /opt/everest/share/everest/ocpp/
 
 if [ -L "/etc/mosquitto/conf.d" ]; then
     echo Rebuilding over existing work directory, skipping mosquitto config magic
