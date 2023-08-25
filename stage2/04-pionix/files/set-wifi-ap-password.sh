35750edd1c0b6fe50ff6e0e192245fede10017e5
Cornelius Claussen <cc@pionix.de>
2023-08-25 18:44:34 +0200

    use passphrase instead of psk
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

--- stage2/04-pionix/files/set-wifi-ap-password.sh
+++ stage2/04-pionix/files/set-wifi-ap-password.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-WIFI_AP_PSK_FILE="/mnt/factory_data/wifi_ap_psk"
+WIFI_AP_PSK_FILE="/mnt/factory_data/wifi_ap_passphrase"
 if [ -f "$WIFI_AP_PSK_FILE" ]; then
     echo "Using wifi_ap_psk file for wifi password"
     WIFI_AP_PSK=`cat $WIFI_AP_PSK_FILE`
-    sed -i "/wpa_psk=/c\wpa_psk=$WIFI_AP_PSK" /etc/hostapd/hostapd.conf
+    sed -i "/wpa_passphrase=/c\wpa_passphrase=$WIFI_AP_PSK" /etc/hostapd/hostapd.conf
 fi
