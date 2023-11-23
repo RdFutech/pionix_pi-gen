19323638c2164fcec5770c8bd442c833fbfde466
Cornelius Claussen <cc@pionix.de>
2023-11-23 11:28:41 +0100

    add missing files
    Signed-off-by: Cornelius Claussen <cc@pionix.de>

---
+++ stage2/16-install-pionix-control/01-run.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -e
+
+install file/umwc-init-mnt-userdata.tgz "${ROOTFS_DIR}/opt/everest/"
+mkdir -p $WORK_DIR/pionixctrl
+
+(
+cd $WORK_DIR/pionixctrl
+git clone git@github.com:PionixInternal/pionix-control.git || true
+cp -r pionix-control ${ROOTFS_DIR}
+cd pionix-control
+git pull || true
+install ./everest-control.service "${ROOTFS_DIR}/lib/systemd/system/"
+install ./pionix-control.service "${ROOTFS_DIR}/lib/systemd/system/"
+)
+
+on_chroot <<EOF
+cd /pionix-control
+pip install --upgrade pip
+pip3 install .
+cd /
+rm -rf /pionix-control
+systemctl enable everest-control.service
+systemctl enable pionix-control.service
+EOF
+
