471e2df657cae450c2acdf408352097fed15f2d7
diskgokey <diskgokey@gmail.com>
2024-03-08 15:42:50 +0100

    ftp no ssl

--- upload_futech.sh
+++ upload_futech.sh
@@ -12,13 +12,14 @@ export SSHPASS=Futech123
 
 echo "Uploading $FN (HWID $HWID/Version $VERSION) to kristof@pt.futech.be/ilucharge2/${HWID}/${UPDATE_CHANNEL}"
 
-lftp -u kristof,Futech123 pt.futech.be << EOF
+lftp -u kristof,Futech123 pt.futech.be -e "set ftp:ssl-allow no;"<< EOF
    cd subdomains/pt/httpdocs/firmware/ilucharge2
    mkdir ${HWID}
    cd ${HWID}
    mkdir ${UPDATE_CHANNEL}
    cd ${UPDATE_CHANNEL}
-   mrm *
+   rm current.meta
+   rm current.img.gz
    put "$FN.pnx"
    put "$FN.meta"
    put "$FN.meta" -o "current.meta"
